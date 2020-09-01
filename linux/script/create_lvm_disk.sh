a2f0433fb392e54e45a1463f94327aae8d418e35
Håkon Klausen <hakon.klausen@ae.no>
2020-09-01 12:15:00 +0200

    Added braces

--- linux/script/create_lvm_disk.sh
+++ linux/script/create_lvm_disk.sh
@@ -8,10 +8,10 @@ volumeGroupName="vg_$volumeName"
 
 # Log request parameters
 echo "Create LVM Disk started..." >>/var/log/lvmlog
-echo "  Volume name:       $volumeName" >>/var/log/lvmlog
-echo "  Volume Group name: $volumeGroupName" >>/var/log/lvmlog
-echo "  MountPoint:        $mountPoint" >>/var/log/lvmlog
-echo "  diskList:          $diskList" >>/var/log/lvmlog
+echo "  Volume name:       ${volumeName}" >>/var/log/lvmlog
+echo "  Volume Group name: ${volumeGroupName}" >>/var/log/lvmlog
+echo "  MountPoint:        ${mountPoint}" >>/var/log/lvmlog
+echo "  diskList:          ${diskList}" >>/var/log/lvmlog
 
 # Install packages
 #apt -y update &>>/var/log/lvmlog
@@ -23,32 +23,32 @@ echo "Partitioning disks..." >>/var/log/lvmlog
 for disk in $diskList 
 do
     echo "  Partitioning $disk..." >>/var/log/lvmlog
-    parted /dev/$disk mklabel gpt mkpart primary 2048s 100% >>/var/log/lvmlog
+    parted /dev/${disk} mklabel gpt mkpart primary 2048s 100% >>/var/log/lvmlog
     partitionList="${partitionList} /dev/${disk}1"
 done
 
-echo "Partition list: $partitionList" >>/var/log/lvmlog
+echo "Partition list: ${partitionList}" >>/var/log/lvmlog
 
 # Create Physical Volumes
 echo "Creating Physical Volumes..." >>/var/log/lvmlog
-pvcreate "$partitionList" >>/var/log/lvmlog
+pvcreate "${partitionList}" >>/var/log/lvmlog
 
 # Create Volume Group
 echo "Creating Volume Group..." >>/var/log/lvmlog
-vgcreate $volumeGroupName "$partitionList" >>/var/log/lvmlog
+vgcreate ${volumeGroupName} "${partitionList}" >>/var/log/lvmlog
 
 # Create Logical colume
 echo "Creating Logical Volume..." >>/var/log/lvmlog
-lvcreate -l 100%VG -n "$volumeName" "$volumeGroupName" >>/var/log/lvmlog
+lvcreate -l 100%VG -n "$volumeName" "${volumeGroupName}" >>/var/log/lvmlog
 
 # Create file system
 echo "Creating File System..." >>/var/log/lvmlog
-mkfs.ext4 /dev/$volumeGroupName/$volumeName >>/var/log/lvmlog
+mkfs.ext4 /dev/${volumeGroupName}/${volumeName} >>/var/log/lvmlog
 
 echo "Mounting file system..." >>/var/log/lvmlog
-echo "/dev/$volumeGroupName/$volumeName $mountPoint ext4 defaults 0 0" >>/etc/fstab
+echo "/dev/${volumeGroupName}/${volumeName} ${mountPoint} ext4 defaults 0 0" >>/etc/fstab
 cat /etc/fstab >>/var/log/lvmlog
-mkdir "$mountPoint" >>/var/log/lvmlog
+mkdir "${mountPoint}" >>/var/log/lvmlog
 mount -a >>/var/log/lvmlog
 
 exit 0
