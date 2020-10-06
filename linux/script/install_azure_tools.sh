b4f0d4d62e9838ea6c5ec4481113b86f14728183
Håkon Klausen <hakon.klausen@ae.no>
2020-10-06 13:31:32 +0200

    apt -y

--- linux/script/install_azure_tools.sh
+++ linux/script/install_azure_tools.sh
@@ -11,20 +11,20 @@ echo "Install Azure tools was here" >$LOGFILE
 # Azure CLI
 #
 echo "Install of Azure CLI started" >>$LOGFILE
-sudo apt-get install ca-certificates curl apt-transport-https lsb-release gnupg
+apt -y install ca-certificates curl apt-transport-https lsb-release gnupg
 curl -sL https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/microsoft.gpg > /dev/null
 AZ_REPO=$(lsb_release -cs)
 echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | sudo tee /etc/apt/sources.list.d/azure-cli.list
-sudo apt-get update
-sudo apt-get install azure-cli
+apt -y update
+apt -y install azure-cli
 # 
 # MSSQL tools
 #
 echo "Install of mssql tools started" >>$LOGFILE
 curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
 curl https://packages.microsoft.com/config/ubuntu/16.04/prod.list | sudo tee /etc/apt/sources.list.d/msprod.list
-sudo apt-get update >>$LOGFILE
-sudo apt-get install mssql-tools unixodbc-dev >>$LOGFILE
+apt -y update >>$LOGFILE
+apt -y install mssql-tools unixodbc-dev >>$LOGFILE
 #
 # azcopy
 #
